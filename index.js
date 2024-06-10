766c9470a2f95a4d36382070cdeceb3af1c6e506
bsrBe <bsr7349@gmail.com>
2024-06-10 20:09:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-function calculateResult() {
-    let expression = document.getElementById('display').innerText;
 
-    try {
-        let result = eval(expression);
-        
-        if (result === Infinity || result === -Infinity) {
-            throw new Error("Error!");
-        }
-
-        document.getElementById('display').innerText = result;
-    } catch (error) {
-        document.getElementById('display').innerText = error.message;
-    }
-}
