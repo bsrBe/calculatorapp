cd5f6a42603968e9211b07e1533474d732757c13
bsrBe <bsr7349@gmail.com>
2024-06-10 20:16:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function calculateResult() {
+    let expression = document.getElementById('display').innerText;
 
+    try {
+        let result = eval(expression);
+        
+        if (result === Infinity || result === -Infinity) {
+            throw new Error("Error!");
+        }
+
+        document.getElementById('display').innerText = result;
+    } catch (error) {
+        document.getElementById('display').innerText = error.message;
+    }
+}
